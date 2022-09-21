239373c302cd7032f6077c782c24ef50cb4758d5
Dmitriy Bezgodko <daimon.94@mail.ru>
2022-09-21 18:17:40 +0300

    add moduls

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -1,347 +1,19 @@
-'use strict';
+import modalWindow from './modules/modal.js';
+import render from './modules/render.js';
+import {getStorage} from './modules/serviceStorage.js';
 
-const data = [
-  {
-    name: 'Иван',
-    surname: 'Петров',
-    phone: '+79514545454',
-  },
-  {
-    name: 'Игорь',
-    surname: 'Семёнов',
-    phone: '+79999999999',
-  },
-  {
-    name: 'Семён',
-    surname: 'Иванов',
-    phone: '+79800252525',
-  },
-  {
-    name: 'Мария',
-    surname: 'Попова',
-    phone: '+79876543210',
-  },
-];
+const {renderPhoneBook, renderContacts} = render;
+
+const {
+  hoverRow,
+  modalControl,
+  deleteControl,
+  formControl,
+} = modalWindow;
 
 {
-  const getStorage = () => {
-    // получает в виде аргумента ключ и
-    // по нему запрашивает данные из localStorage
-    // и возвращает их, если их нет, то возвращает пустой массив
-    const arr = localStorage.getItem('phonebook') ?
-      JSON.parse(localStorage.getItem('phonebook')) : [];
-    return arr;
-  };
-
-  const setStorage = (key, value) => {
-    // получает ключ и объект в виде аргументов и
-    // дописывает данные в localStorage.
-    // Для этого с помощью getStorage необходимо данные получить,
-    // дописать объект в массив и отправить после этого данные в localStorage
-    const obj = JSON.stringify(value);
-    localStorage.setItem(key, obj);
-  };
-  const removeStorage = (tel, newData) => {
-    // получает в виде аргумента номер телефона,
-    // и удаляет контакт из localStorage,
-    // с логикой необходимо разобраться самостоятельно!
-    for (let i = 0; i < newData.length; i++) {
-      if (tel === newData[i].phone) {
-        newData.splice(i, 1);
-      }
-    }
-    setStorage('phonebook', newData);
-  };
-
-  const createContainer = () => {
-    const container = document.createElement('div');
-    container.classList.add('container');
-    return container;
-  };
-
-  const createHeader = () => {
-    const header = document.createElement('header');
-    header.classList.add('header');
-
-    const headerContainer = createContainer();
-    header.append(headerContainer);
-    header.headerContainer = headerContainer;
-
-    return header;
-  };
-
-  const createLogo = title => {
-    const h1 = document.createElement('h1');
-    h1.classList.add('logo');
-    h1.textContent = `Телефонный справочник. ${title}`;
-    return h1;
-  };
-
-  const createMain = () => {
-    const main = document.createElement('main');
-    const mainContainer = createContainer();
-    main.append(mainContainer);
-    main.mainContainer = mainContainer;
-    return main;
-  };
-
-  const createButtonsGroup = params => {
-    const btnWrapper = document.createElement('div');
-    btnWrapper.classList.add('btn-wrapper');
-
-    const btns = params.map(({ classList, type, text }) => {
-      const button = document.createElement('button');
-      button.type = type;
-      button.textContent = text;
-      button.className = classList;
-      return button;
-    });
-
-    btnWrapper.append(...btns);
-
-    return {
-      btnWrapper,
-      btns,
-    };
-  };
-
-  const createTable = () => {
-    const table = document.createElement('table');
-    table.classList.add('table', 'table-striped');
-
-    const thead = document.createElement('thead');
-    thead.insertAdjacentHTML('beforeend', `
-    <tr>
-    <th class="delete">Удалить</th>
-    <th>Имя</th>
-    <th>Фамилия</th>
-    <th>Телефон</th>
-    </tr>
-    `);
-
-    const tbody = document.createElement('tbody');
-
-    table.append(thead, tbody);
-    table.tbody = tbody;
-
-    return table;
-  };
-
-  const createForm = () => {
-    const overlay = document.createElement('div');
-    overlay.classList.add('form-overlay');
-
-    const form = document.createElement('form');
-    form.classList.add('form');
-    form.insertAdjacentHTML('beforeend', `
-    <button class="close" type="button"></button>
-    <h2 class="form-title">Добавить контакт</h2>
-    <div class="form-group">
-      <label class="form-label" for="name">Имя:</label>
-      <input class="form-input" name="name" id="name" type="text" required>
-    </div>
-    <div class="form-group">
-      <label class="form-label" for="surname">Фамилия:</label>
-      <input class="form-input" name="surname" 
-      id="surname" type="text" required>
-    </div>
-    <div class="form-group">
-      <label class="form-label" for="phone">Телефон:</label>
-      <input class="form-input" name="phone" id="phone" type="number" required>
-    </div>
-    `);
-
-    const buttonGroup = createButtonsGroup([
-      {
-        classList: 'btn btn-primary mr-3',
-        type: 'submit',
-        text: 'Добавить',
-      },
-      {
-        classList: 'btn btn-danger',
-        type: 'reset',
-        text: 'Отмена',
-      },
-    ]);
-
-    form.append(...buttonGroup.btns);
-    overlay.append(form);
-
-    return {
-      overlay,
-      form,
-    };
-  };
-
-  const createFooter = () => {
-    const footer = document.createElement('footer');
-    const footerContainer = createContainer();
-    footer.append(footerContainer);
-    footer.footerContainer = footerContainer;
-    footer.footerContainer.insertAdjacentHTML('beforeend', `
-      <p>Все права защищены &#169;Максим</p>
-    `);
-    footer.classList.add('footer');
-    return footer;
-  };
-
-  const renderPhoneBook = (app, title) => {
-    const header = createHeader();
-    const logo = createLogo(title);
-    const main = createMain();
-    const buttonGroup = createButtonsGroup([
-      {
-        classList: 'btn btn-primary mr-3',
-        type: 'button',
-        text: 'Добавить',
-      },
-      {
-        classList: 'btn btn-danger',
-        type: 'button',
-        text: 'Удалить',
-      },
-    ]);
-    const table = createTable();
-
-    const { form, overlay } = createForm();
-
-    const footer = createFooter();
-
-    header.headerContainer.append(logo);
-    main.mainContainer.append(buttonGroup.btnWrapper, table, overlay);
-    app.append(header, main, footer);
-
-    return {
-      list: table.tbody,
-      logo,
-      btnAdd: buttonGroup.btns[0],
-      btnDel: buttonGroup.btns[1],
-      formOverlay: overlay,
-      form,
-    };
-  };
-
-  const createRow = ({ name: firstName, surname, phone }) => {
-    const tr = document.createElement('tr');
-    tr.classList.add('contact');
-
-    const tdDel = document.createElement('td');
-    tdDel.classList.add('delete');
-    const buttonDel = document.createElement('button');
-    buttonDel.classList.add('del-icon');
-    tdDel.append(buttonDel);
-
-    const tdName = document.createElement('td');
-    tdName.textContent = firstName;
-
-    const tdSurname = document.createElement('td');
-    tdSurname.textContent = surname;
-
-    const tdPhone = document.createElement('td');
-    const phoneLink = document.createElement('a');
-    phoneLink.href = `tel:${phone}`;
-    phoneLink.textContent = phone;
-    tr.phoneLink = phoneLink;
-    tr.id = phoneLink.textContent;
-    tdPhone.append(phoneLink);
-
-    tr.append(tdDel, tdName, tdSurname, tdPhone);
-
-    return tr;
-  };
-
-  const renderContacts = (elem, data, newData) => {
-    let allRow = [];
-    if (newData !== []) {
-      allRow = newData.map(createRow);
-    } else {
-      allRow = data.map(createRow);
-    }
-    console.log(allRow);
-    elem.append(...allRow);
-    return allRow;
-  };
-
-  const hoverRow = (allRow, logo) => {
-    const text = logo.textContent;
-    allRow.forEach(contact => {
-      contact.addEventListener('mouseenter', () => {
-        logo.textContent = contact.phoneLink.textContent;
-      });
-      contact.addEventListener('mouseleave', () => {
-        logo.textContent = text;
-      });
-    });
-  };
-
-  const modalControl = (btnAdd, formOverlay) => {
-    const openModal = () => {
-      formOverlay.classList.add('is-visible');
-    };
-
-    const closeModal = () => {
-      formOverlay.classList.remove('is-visible');
-    };
-
-    btnAdd.addEventListener('click', openModal);
-
-    formOverlay.addEventListener('click', e => {
-      const target = e.target;
-      if (target === formOverlay || target.closest('.close')) {
-        closeModal();
-      }
-    });
-
-    return {
-      closeModal,
-    };
-  };
-
-  const deleteControl = (btnDel, list, newData) => {
-    btnDel.addEventListener('click', () => {
-      document.querySelectorAll('.delete').forEach(del => {
-        del.classList.toggle('is-visible');
-      });
-    });
-
-    list.addEventListener('click', e => {
-      const target = e.target;
-      if (target.closest('.del-icon')) {
-        const contact = target.closest('.contact');
-        const tel = contact.id;
-        contact.remove();
-        removeStorage(tel, newData);
-      }
-    });
-  };
-
-  const addContactData = (contact, newData) => {
-    data.push(contact);
-    newData.push(contact);
-    setStorage('phonebook', newData);
-  };
-
-  const addContactPage = (contact, list) => {
-    list.append(createRow(contact));
-  };
-
-  const formControl = (form, list, closeModal, newData) => {
-    form.addEventListener('submit', e => {
-      e.preventDefault();
-
-      const formData = new FormData(e.target);
-      const newContact = Object.fromEntries(formData);
-      addContactPage(newContact, list);
-      addContactData(newContact, newData);
-      form.reset();
-      closeModal();
-    });
-  };
-
   const init = (selectorApp, title) => {
     const app = document.querySelector(selectorApp);
-
-
     const {
       list,
       logo,
@@ -350,23 +22,20 @@ const data = [
       formOverlay,
       form,
     } = renderPhoneBook(app, title);
+
     // Функционал
 
-    // надо функции из урока 8 перевести в переменные где-то здесь
-    const newData = getStorage();
-    // const delTel = 
-    console.log('newData: ', newData);
+    const data = getStorage();
 
-    const allRow = renderContacts(list, data, newData);
+    const allRow = renderContacts(list, data);
 
     const { closeModal } = modalControl(btnAdd, formOverlay);
 
     hoverRow(allRow, logo);
-    deleteControl(btnDel, list, newData);
-    formControl(form, list, closeModal, newData);
+    deleteControl(btnDel, list, data);
+    formControl(form, list, closeModal, data);
   };
 
 
   window.phoneBookInit = init;
 }
-
